739a1183eb7904ba05a455edbd3b27d132687a2a
Phu Nguyen <phuga@example.com>
2026-05-24 19:37:44 +0700

    chore/backend: update backend file console.php

--- BE/routes/console.php
+++ BE/routes/console.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('borrowings:calculate-fines')->dailyAt('00:05');
+Schedule::command('app:send-due-soon-warnings')->dailyAt('08:00');
+Schedule::command('app:send-overdue-warnings')->dailyAt('08:05');
+Schedule::command('borrowings:cleanup-approved')->hourly();
+Schedule::command('room-bookings:cleanup-no-show')->everyFiveMinutes();
+Schedule::command('room-bookings:complete')->everyTenMinutes();
