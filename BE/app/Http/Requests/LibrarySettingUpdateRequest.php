3456a1432db247f29950fd26c7d275a08fcff7f2
Phu Nguyen <phuga@example.com>
2026-05-24 19:37:37 +0700

    feat/request: add validation rules in LibrarySettingUpdateRequest

--- BE/app/Http/Requests/LibrarySettingUpdateRequest.php
+++ BE/app/Http/Requests/LibrarySettingUpdateRequest.php
@@ -16,6 +16,18 @@ class LibrarySettingUpdateRequest extends FormRequest
         return [
             'loan_period_days' => ['required', 'integer', 'min:1', 'max:365'],
             'max_active_loans' => ['required', 'integer', 'min:1', 'max:50'],
+            'fine_per_day' => ['required', 'numeric', 'min:0', 'max:1000000'],
+            'max_fine_per_loan' => ['required', 'numeric', 'min:0', 'max:10000000'],
+            'grace_period_days' => ['required', 'integer', 'min:0', 'max:30'],
+            'room_max_hours_per_booking' => ['required', 'integer', 'min:1', 'max:12'],
+            'room_max_bookings_per_day' => ['required', 'integer', 'min:1', 'max:10'],
+            'room_advance_booking_days' => ['required', 'integer', 'min:1', 'max:30'],
+            'room_min_group_size' => ['required', 'integer', 'min:1', 'max:20'],
+            'room_checkin_window_minutes' => ['required', 'integer', 'min:5', 'max:60'],
+            'room_booking_requires_approval' => ['required', 'boolean'],
+            'room_open_time' => ['required', 'string', 'regex:/^[0-2][0-9]:[0-5][0-9]$/'],
+            'room_close_time' => ['required', 'string', 'regex:/^[0-2][0-9]:[0-5][0-9]$/'],
+            'room_cancel_deadline_hours' => ['required', 'integer', 'min:0', 'max:24'],
         ];
     }
 }
